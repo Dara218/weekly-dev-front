6d9b5b2352981a628ee1e7fdf58a53c90cb0c539
RalphYNS <ralphbautista.yns@gmail.com>
2025-12-05 21:25:08 +0800

    WEEKLY-DEV-2 - Create Authentication Page | Login and Forgot Password - Added css styles for:  - button  - container  - form-container  - input-fields  - style  - text  - wrapper - Added login and reset password routes - Created Login and Reset Password ui

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,20 @@
-import LandingPage from '@/views/pages/LandingPage.vue';
+import Login from '@/views/pages/authentication/Login.vue';
+import ResetPassword from '@/views/pages/authentication/ResetPassword.vue';
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'landing-page',
-      component: LandingPage,
+      path: '/login',
+      name: 'login',
+      component: Login,
+      alias: '/',
+    },
+    {
+      path: '/reset-password',
+      name: 'reset-password',
+      component: ResetPassword,
     }
   ],
 })
---
+++ src/views/pages/authentication/ResetPassword.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="card-page-container">
+    <div class="card-page-wrapper">
+      <!-- Header -->
+      <div class="text-center">
+        <h1 class="page-title">FORGOT PASSWORD</h1>
+      </div>
+
+      <!-- Form -->
+      <form @submit.prevent="handleResetRequest" class="card-container-form">
+        <div class="space-y-4">
+          <!-- Email Field -->
+          <div>
+            <label for="email" class="form-label">
+              Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              required
+              class="input-field"
+              placeholder="Enter your email"
+            />
+          </div>
+        </div>
+
+        <!-- Success Message -->
+        <p v-if="successMessage" class="success-message">
+          {{ successMessage }}
+        </p>
+
+        <!-- Buttons -->
+        <div class="button-container-2">
+          <button
+            type="submit"
+            class="button-submit"
+          >
+            Send Reset Link
+          </button>
+          <router-link
+            to="/login"
+            class="button-common"
+          >
+            Back to Login
+          </router-link>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  // 
+</script>
